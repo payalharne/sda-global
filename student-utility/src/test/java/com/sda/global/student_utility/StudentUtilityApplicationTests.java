7911a1b31bacd9904485c5ad3b72d833d4690e93
ksrandive <ksrandive@gmail.com>
2025-07-26 12:17:56 +0530

    added repos

---
+++ student-utility/src/test/java/com/sda/global/student_utility/StudentUtilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sda.global.student_utility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentUtilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
